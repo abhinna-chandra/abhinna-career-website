1d74112c05381f9fc79e59f53ae051b852a4b79b
Abhinna Chandra Biswal <92629544+abhinna-chandra@users.noreply.github.com>
2023-03-29 15:30:11 +0000

    Initial Commit

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+print("My name is Abhinna")
 
 
 @app.route("/")
 def hello_world():
-  return "Hello Abhinna!"
-
-  print(__name__)
-  if __name__ == "__main__":
-    print("I am inside loop.")
+  return "<p>Hello, World!</p>"
